348c6b6e195ddaaf0bc60689c2723413fad2fc55
Godfrey Lebo <emorylebo@gmail.com>
2022-09-28 09:44:40 +0100

    Create user schema

---
+++ .history/server/models/user_20220928094349.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true},
+    name: {type: String, required: true},
+    name: {type: String, required: true},
+})
---
+++ .history/server/models/user_20220928094407.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    googleId: {type: String, required: true},
+})
