967f7dd1bd2ed517d97524edd15ae58cf7532d91
Godfrey Lebo <emorylebo@gmail.com>
2022-09-28 07:37:35 +0100

    Project Initialization and server setup

---
+++ .history/server/index_20220928073002.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const port = 5000;
+
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.send();
+});
+
+app.listen
---
+++ .history/server/index_20220928073051.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const port = 5000;
+
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.send();
+});
+
+app.listen(port, ()=>{
+    console.log("Server running on port "+port);
+});
